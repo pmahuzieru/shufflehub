20c6e763bf1ef9231033e29606b860b0d23ff487
Pablo <pmahuzieru@gmail.com>
2024-10-28 18:10:09 -0300

    Add Register and Profile views in users

--- shufflehub/users/views.py
+++ shufflehub/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = UserCreationForm()
+    return render(
+        request=request, template_name="users/register.html", context={"form": form}
+    )
+
+
+@login_required
+def profile(request):
+    return render(request=request, template_name="users/profile.html")
